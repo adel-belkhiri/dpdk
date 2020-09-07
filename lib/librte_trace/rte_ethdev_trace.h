9fabf80e6b534e4b9a2cc424c0fe470383ca96d3
adel-belkhiri <adel.belkhiri@gmail.com>
2020-09-06 23:35:44 -0400

    Adding some new tracepoints to librte_ethdev

--- lib/librte_trace/rte_ethdev_trace.h
+++ lib/librte_trace/rte_ethdev_trace.h
@@ -8,8 +8,24 @@
 #define _RTE_ETHDEV_TRACE_H
 
 #include <lttng/tracepoint.h>
+#include <rte_ethdev.h>
 
+/**
+ * The functions exported by the application Ethernet API to setup a device
+ * designated by its port identifier must be invoked in the following order:
+ *     - rte_eth_dev_configure()
+ *     - rte_eth_tx_queue_setup()
+ *     - rte_eth_rx_queue_setup()
+ *     - rte_eth_dev_start()
+ *
+ * (For documentation, please read the file rte_ethdev.h )
+ */
 
+/**
+ * After probing the PCI bus, create an Ethernet device
+ * @p port_id: Ethernet device identifier.
+ * @p name: Name of the device
+ */
 TRACEPOINT_EVENT(
     librte_ethdev,
     rte_eth_dev_create,
@@ -23,6 +39,108 @@ TRACEPOINT_EVENT(
     )
 )
 
+/**
+ * Configure an Ethernet device.
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_configure,
+    TP_ARGS(
+        const struct rte_eth_dev*, dev,
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer_hex(void*, dev, dev)
+        ctf_integer(uint16_t, port_id, port_id)
+        ctf_string(name, dev->data->name)
+        ctf_integer(uint16_t, nb_rx_q, dev->data->nb_rx_queues)
+        ctf_integer(uint16_t, nb_tx_q, dev->data->nb_tx_queues)
+        ctf_integer(uint64_t, rx_offloads, dev->data->dev_conf.rxmode.offloads)
+        ctf_integer(uint64_t, tx_offloads, dev->data->dev_conf.txmode.offloads)
+        ctf_integer(uint32_t, max_rx_pkt_len, dev->data->dev_conf.rxmode.max_rx_pkt_len)
+    )
+)
+
+/**
+ * Start an Ethernet device.
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_start,
+    TP_ARGS(
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer(uint16_t, port_id, port_id)
+    )
+)
+
+/**
+ * Stop an Ethernet device. The device can be restarted with a call to
+ * rte_eth_dev_start()
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_stop,
+    TP_ARGS(
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer(uint16_t, port_id, port_id)
+    )
+)
+
+/**
+ * Link up an Ethernet device.
+ *
+ * Set device link up will re-enable the device rx/tx
+ * functionality after it is previously set device linked down.
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_set_link_up,
+    TP_ARGS(
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer(uint16_t, port_id, port_id)
+    )
+)
+
+/**
+ * Link down an Ethernet device.
+ * The device rx/tx functionality will be disabled if success, and it
+ * can be re-enabled with a call to rte_eth_dev_set_link_up()
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_set_link_down,
+    TP_ARGS(
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer(uint16_t, port_id, port_id)
+    )
+)
+
+/**
+ * Close a stopped Ethernet device. The device cannot be restarted!
+ * The resources are free but the port id is kept.
+ */
+TRACEPOINT_EVENT(
+    librte_ethdev,
+    rte_eth_dev_close,
+    TP_ARGS(
+        uint16_t, port_id
+    ),
+    TP_FIELDS(
+        ctf_integer(uint16_t, port_id, port_id)
+    )
+)
+
+/**
+ * Close the device and free all its resources.
+ */
 TRACEPOINT_EVENT(
     librte_ethdev,
     rte_eth_dev_destroy,
@@ -34,6 +152,9 @@ TRACEPOINT_EVENT(
     )
 )
 
+/**
+ * Allocate and set up a transmit queue for an Ethernet device.
+ */
 TRACEPOINT_EVENT(
     librte_ethdev,
     rte_eth_tx_queue_setup,
@@ -47,10 +168,13 @@ TRACEPOINT_EVENT(
         ctf_integer(uint16_t, port_id, port_id)
         ctf_integer(uint16_t, queue_id, queue_id)
         ctf_integer(uint16_t, nb_desc, nb_desc)
-        ctf_integer(unsigned int, socket_id, socket_id)                
+        ctf_integer(unsigned int, socket_id, socket_id)
     )
 )
 
+/**
+ * Allocate and set up a reception queue for an Ethernet device.
+ */
 TRACEPOINT_EVENT(
     librte_ethdev,
     rte_eth_rx_queue_setup,
@@ -64,7 +188,7 @@ TRACEPOINT_EVENT(
         ctf_integer(uint16_t, port_id, port_id)
         ctf_integer(uint16_t, queue_id, queue_id)
         ctf_integer(uint16_t, nb_desc, nb_desc)
-        ctf_integer(unsigned int, socket_id, socket_id)                
+        ctf_integer(unsigned int, socket_id, socket_id)
     )
 )
 
@@ -77,8 +201,8 @@ TRACEPOINT_EVENT(
     ),
     TP_FIELDS(
         ctf_integer(uint16_t, port_id, port_id)
-        ctf_integer(uint16_t, queue_id, queue_id)            
-    )    
+        ctf_integer(uint16_t, queue_id, queue_id)
+    )
 )
 
 TRACEPOINT_EVENT(
@@ -90,7 +214,7 @@ TRACEPOINT_EVENT(
     ),
     TP_FIELDS(
         ctf_integer(uint16_t, port_id, port_id)
-        ctf_integer(uint16_t, queue_id, queue_id)            
+        ctf_integer(uint16_t, queue_id, queue_id)
     )
 )
 
@@ -104,7 +228,7 @@ TRACEPOINT_EVENT(
     ),
     TP_FIELDS(
         ctf_integer(uint16_t, port_id, port_id)
-        ctf_integer(uint16_t, queue_id, queue_id)            
+        ctf_integer(uint16_t, queue_id, queue_id)
     )
 )
 
@@ -117,9 +241,10 @@ TRACEPOINT_EVENT(
     ),
     TP_FIELDS(
         ctf_integer(uint16_t, port_id, port_id)
-        ctf_integer(uint16_t, queue_id, queue_id)            
+        ctf_integer(uint16_t, queue_id, queue_id)
     )
 )
+
 #endif /* _RTE_ETHDEV_TRACE_H */
 
 #include <lttng/tracepoint-event.h>
